ea9520b1b504a97c26344866a8ef1ffd285341e1
Asad Ali Bhatti <asadalibhatti@hotmail.com>
2018-05-30 03:26:03 +0200

    ttn listener is working

--- config/initializers/ttn_listener.rb
+++ config/initializers/ttn_listener.rb
@@ -1,3 +1,17 @@
 Thread.new do
-  
+  client = MQTT::Client.connect(host: 'eu.thethings.network', port: 1883,
+                                username: ENV["TTN_APP_USERNAME"], password: ENV["TTN_APP_PASSWORD"])
+  if client.connected?
+    p "#...Connection to TTN established...#"
+    client.subscribe("#{ENV["TTN_APP_USERNAME"]}/devices/12345/up")
+
+    client.get do |topic, message|
+      message = JSON.parse message
+      p "####### MESSAGE FROM THE THINGS NETWORK "
+      p "Parking available: #{ message['payload_fields']['available']}"
+      p "Sensor ID: #{ message['payload_fields']['sensor'] }"
+    end
+  else
+    p "#...Connection to TTN Failed!...#"
+  end
 end
